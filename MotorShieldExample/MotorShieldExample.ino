fb7c91582847f218b94795babd7d2529b6c2daa7
UWNRG Controls <uwnrg.controls@gmail.com>
2017-12-19 21:36:25 -0500

    Receive data from minotaur and break up UP and RIGHT directions

--- MotorShieldExample/MotorShieldExample.ino
+++ MotorShieldExample/MotorShieldExample.ino
@@ -6,6 +6,9 @@
 // sol analogwrite controls strength of the solenoid
 // brake digital pins control auto shut off
 
+#define BAUD_RATE 9600
+
+
 //X-axis corresponds to pin A
 const int controlX = 12;
 const int solX = 3;
@@ -16,9 +19,18 @@ const int controlY = 11;
 const int solY = 13;
 const int brakeY = 8;
 
+// TODO: make this an enum
+const short UP = 0b110001;
+const short RIGHT = 0b110010;
 
+// Input bytes from Controls
+int incomingByte;
 
 void setup () {
+  // Initialize serial
+  Serial.begin(BAUD_RATE);
+  while(!Serial) {}
+  
   //Initialization of all digital and analog pinouts to control motor shield
   pinMode(controlX, OUTPUT);
   pinMode(solX, OUTPUT);
@@ -33,6 +45,36 @@ void setup () {
   digitalWrite(controlY, HIGH);
 }
 
+void moveX(short dir) {
+  if (dir != RIGHT) return;   // TODO: make left direction work
+  
+  Serial.println("Moving RIGHT");
+  
+  // Set strength of solenoid using PWM, can be any value [0, 255]
+  analogWrite(solX, 255);
+  analogWrite(solY, 0);
+
+  // Turn brake on or off to allow current to flow through pins
+  digitalWrite(brakeX, LOW);
+  digitalWrite(brakeY, HIGH);
+  
+  // Movement time
+  delay(3000); 
+}
+
+void moveY(short dir) {
+  if (dir != UP) return;   // TODO: make down direction work
+  
+  Serial.println("Moving UP");
+  
+  analogWrite(solX, 0);
+  analogWrite(solY, 255);
+  
+  digitalWrite(brakeX, HIGH);
+  digitalWrite(brakeY, LOW);
+  
+  delay(3000);
+}
 
 void loop () {
   // Voltage: 3.21 V
@@ -40,22 +82,15 @@ void loop () {
   // Delay Time: 10 ms
   
   // Set strength of the solenoid using PWM, can be any value [0,255]
-  
   analogWrite(solX, 255);
   analogWrite(solY, 0);
   
-  // Turn brake on or off to allow current to flow through pin
-  
-  digitalWrite(brakeX, LOW);
-  digitalWrite(brakeY, HIGH);
-  
-  delay(3000);cd 
-  
-
-  analogWrite(solX, 0);
-  analogWrite(solY, 255);
-  
-  digitalWrite(brakeX, HIGH);
-  digitalWrite(brakeY, LOW);
-  delay(3000);
+  if( Serial.available() ) {
+    incomingByte = Serial.read();
+    
+    Serial.println(incomingByte, BIN);
+    // TODO: take one byte and separate it properly to X and Y directions
+    moveX(incomingByte);
+    moveY(incomingByte);
+  }
 }
