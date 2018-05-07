9bb5d7d2dd6dc87b6b2438f8c7d4c0e4ce601bd0
Brett <bretskyselby99@gmail.com>
2018-05-07 01:12:19 -0400

    Enable changing power levels

--- test/jeff_test/jeff_test.ino
+++ test/jeff_test/jeff_test.ino
@@ -4,6 +4,7 @@
 #define RIGHT 100
 #define DOWN  115
 #define UP    119
+#define POWER 112
 
 #define sol_a 3
 #define sol_b 6
@@ -14,6 +15,11 @@
 #define sol_right sol_d
 #define sol_down  sol_b
 #define sol_up    sol_a
+
+int power[4] = {255, 255, 255, 255}; //UP DOWN LEFT RIGHT
+int solenoids[4] = {sol_up, sol_down, sol_left, sol_right};
+int state = 0; //0 is default, 1 means getting solenoid, 2 means getting power level
+int solenoid = 0;
 
 void setup() {
   pinMode(sol_a, OUTPUT);
@@ -32,40 +38,74 @@ void setup() {
 }
 
 void pulse_sol(int sol_pin, int power = 255) {
+  Serial.print("Pulsing with power:");
+  Serial.print(power);
+  Serial.print('\n');
   analogWrite(sol_pin, power);
   delay(50);//was 10
   analogWrite(sol_pin, 0);
 }
 
-void handle_msg(byte msg) {
+int read_msg(byte msg) {
   switch (msg) {
     case UP:
       Serial.println("UP");
-      pulse_sol(sol_up);
-      break;
+      return 0;
     case DOWN:
-      Serial.println("DOWM");
-      pulse_sol(sol_down);
-      break;
+      Serial.println("DOWN");
+      return 1;
     case LEFT:
       Serial.println("LEFT");
-      pulse_sol(sol_left);
-      break;
+      return 2;
     case RIGHT:
       Serial.println("RIGHT");
-      pulse_sol(sol_right);
-      break;
+      return 3;
+    case POWER:
+      Serial.println("CHANGE POWER LEVEL");
+      return 4;
     default:
       Serial.println("Unknown message");
-      break;
+      return -1;
   }
+}
+
+int handle_msg(byte msg, int power[4], int solenoids[4]) {
+  int id = read_msg(msg);
+  if (id >= 0 && id < 4) { 
+    pulse_sol(solenoids[id], power[id]);
+  }
+  return id;
 }
 
 void loop() {
   if (Serial.available()) {
-    byte msg = Serial.read();
-    Serial.println(msg, BIN);
-    handle_msg(msg);
+    Serial.println("----");
+    if (state == 0) {
+      byte msg = Serial.read();
+      Serial.println(msg, BIN);
+      if (handle_msg(msg, power, solenoids) == 4) {
+        state = 1;
+      }
+    } else if (state == 1) {
+      byte msg = Serial.read();
+      solenoid = read_msg(msg);
+      if (solenoid >= 0 && solenoid < 4) {
+        state = 2;
+        Serial.println("Changing solenoid power");
+      } else {
+        solenoid = 0;
+        state = 0;
+        Serial.println("Invalid solenoid");
+      }
+    } else if (state == 2) {
+      byte msg = Serial.read();
+      int value = msg;
+      Serial.print("New solenoid power: ");
+      Serial.print(value);
+      Serial.print('\n');
+      power[solenoid] = value;
+      state = 0;
+    }
   }
 }
 
